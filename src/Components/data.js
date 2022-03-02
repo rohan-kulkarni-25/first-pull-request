f80bdd43e071165d7678ee81e6689f828f2b3c43
Tejas <75026048+Tejas-117@users.noreply.github.com>
2022-03-02 11:43:49 +0530

    MInor bug fixes and data addition
    Fixed minor bugs

--- src/Components/data.js
+++ src/Components/data.js
@@ -4,6 +4,11 @@ let data = [
     linkedin: "https://www.linkedin.com/in/rohan-k-2502/",
     github: "https://github.com/rohan-kulkarni-25",
   },
+  {
+    name: "Tejas.C.S",
+    linkedin: "https://www.linkedin.com/in/tejas-cs-3a7218232/",
+    github: "https://github.com/Tejas-117",
+  },
 ];
 
 export default data;
