b9cde25701a7240d0edecece4caa2588d45615c0
Jashan sambyal <hikeakh223@gmail.com>
2022-04-14 14:42:05 +0530

    Added data

--- src/Components/data.js
+++ src/Components/data.js
@@ -5,28 +5,31 @@ let data = [
     github: "https://github.com/rohan-kulkarni-25",
   },
   {
-         name: "Deena",
-         linkedin: "https://www.linkedin.com/in/deena-m-2338951a7",
-        github: "https://github.com/Deenadinn",
-      },
+    name: "Deena",
+    linkedin: "https://www.linkedin.com/in/deena-m-2338951a7",
+    github: "https://github.com/Deenadinn",
+  },
   {
     name: "Tejas.C.S",
     linkedin: "https://www.linkedin.com/in/tejas-cs-3a7218232/",
     github: "https://github.com/Tejas-117",
   },
-    {
+  {
     name: "Saikat Mazumder",
     linkedin: "https://www.linkedin.com/in/saikat-mazumder-05a102210/",
     github: "https://github.com/codernoob1",
   },
-    {
+  {
     name: "Aditi Tomar",
     linkedin: "https://www.linkedin.com/in/aditi-tomar-969754206/",
     github: "https://github.com/aditiitomar",
   },
+  {
+    name: "Jashan Sambyal",
+    linkedin: "https://www.linkedin.com/in/jashan-sambyal-b1188a1b2/",
+    github: "https://github.com/Jashan143",
+  },
 ];
-
-  
 
 export default data;
 //   {
